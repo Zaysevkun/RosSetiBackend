ab5451b883453b421a18cffa980c2f1b29e8d179
kilyichev <kilyichev@croc.ru>
2020-11-28 02:41:19 +0300

    Fix admin

--- api/admin.py
+++ api/admin.py
@@ -1,28 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
 from api.models import User, Category, Question, Comment, Request
-
-
-@admin.register(User)
-class UserAdmin(DjangoUserAdmin):
-    """Define admin model for custom User model with no email field."""
-
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('full_name',)}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
-    )
-    list_display = ('email', 'full_name')
-    search_fields = ('email', 'full_name')
-    ordering = ('email',)
-
 
 admin.site.register(Category)
 admin.site.register(Question)
 admin.site.register(Comment)
 admin.site.register(Request)
+admin.site.register(User)
